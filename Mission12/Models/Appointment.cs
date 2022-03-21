5707cc7ecd86b5a83b4444d123b15679a75e97fc
Camik13 <camikchristensen@gmail.com>
2022-03-21 14:21:16 -0600

    Appointment Model

---
+++ Mission12/Models/Appointment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.AspNetcore.Mvc.ModelBinding;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+namespace mission12.Models
+{
+	public Appointment()
+	{
+		[Key]
+		public int AppointmentID { get; set; }
+		public string Name { get; set; }
+		public int Size { get; set; }
+		public string Email { get; set; }
+		public int Phone { get; set; }
+		public string ApptTime { get; set; }
+		
+		
+	}
+}
